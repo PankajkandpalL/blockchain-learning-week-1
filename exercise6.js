7398efe5c1c8c4276d1ecc59999df1eab532d0da
Pankaj Kandpal <pankajkandpal5602@gmail.com>
2024-03-27 03:10:40 +0530

    Update exercise6.js : added comments

--- exercise6.js
+++ exercise6.js
@@ -2,6 +2,7 @@ const calcFactorial = (n) => {
     if(n==0 || n==1) {
         return 1;
     } else {
+         // If the number is not 0 or 1, calculate the factorial recursively by multiplying the number with the factorial of (n-1)
         return n * calcFactorial(n-1);
     }
 }
