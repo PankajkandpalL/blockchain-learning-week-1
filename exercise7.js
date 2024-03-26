9fec9037da7ac7f72e2c020cc542196ee079268a
Pankaj Kandpal <pankajkandpal5602@gmail.com>
2024-03-27 03:12:41 +0530

    Update exercise7.js : updated comment

--- exercise7.js
+++ exercise7.js
@@ -4,6 +4,7 @@ const fibonacciSeq = (n) => {
     } else if (n === 1) {
       return 1;
     } else {
+        // If n is neither 0 nor 1, calculate the Fibonacci sequence recursively and add
       return fibonacciSeq(n - 1) + fibonacciSeq(n - 2);
     }
 }
